aecc3ab872954bd26f9d8f6921a96fbcfb6dc55e
Mykolas25 <justmikis@gmail.com>
2017-02-12 16:33:08 +0200

    Update index.php

--- knygusarasas/index.php
+++ knygusarasas/index.php
@@ -1,7 +1,4 @@
 <?php
-if (isset($_POST['pk'])){
-	$pk = $_POST['pk'];
-};
 $db = mysqli_connect("localhost", "root", "", "knygu_sarasas") or die(mysqli_error());
 $sql="SELECT * FROM `knygos`";
 $result=mysqli_query($db, $sql) or die ("error: ".mysqli_error());
