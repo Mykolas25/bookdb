b121a04b24e176eec5c44e220aa9e67c5b368e25
Mykolas25 <justmikis@gmail.com>
2017-02-12 16:42:18 +0200

    Update knygos.php

--- knygusarasas/knygos.php
+++ knygusarasas/knygos.php
@@ -5,7 +5,7 @@ if(isset($_GET["pk"])) {
 
 $db = mysqli_connect("localhost", "root", "", "knygu_sarasas") or die(mysqli_error());
 $sql = "SELECT * FROM `knygos` WHERE tableId = $pk";
-$result=mysqli_query($db, $sql) or die ("error: ".mysqli_error());
+$result = mysqli_query($db, $sql) or die (mysqli_error());
 			
 while ($row = mysqli_fetch_array($result)){
 	include "knygusarasas.phtml";
