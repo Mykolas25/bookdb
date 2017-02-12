67fd76f512a515453058b825f096cb820e778804
Mykolas25 <justmikis@gmail.com>
2017-02-12 16:21:10 +0200

    Add files via upload

---
+++ knygusarasas/knygos.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_GET["pk"])) {
+$pk = $_GET["pk"];
+};
+
+$db = mysqli_connect("localhost", "root", "", "knygu_sarasas") or die(mysqli_error());
+$sql = "SELECT * FROM `knygos` WHERE tableId = $pk";
+$result=mysqli_query($db, $sql) or die ("error: ".mysqli_error());
+			
+while ($row = mysqli_fetch_array($result)){
+	include "knygusarasas.phtml";
+};
+?>
